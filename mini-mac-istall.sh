16613d846469eb3839fc25077e657327562be0bb
Saharochekl <izjuha@gmail.com>
2025-06-05 19:16:17 +0300

    Update script for macOS

---
+++ mini-mac-istall.sh
@@ -0,0 +1,7 @@
+echo "copying lib2fc.a to /usr/local/lib"
+cp /lib2fc_Mac/lib2fc.a /usr/local/lib
+
+echo "copying f2c.h to /usr/local/include"
+cp /lib2fc_Mac/f2c.h /usr/local/include
+
+echo "Done"
